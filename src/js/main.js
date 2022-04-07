25268b8674cec47d72948300a08981ba2ed97bc8
Aimarest <amarcosestevez@gmail.com>
2022-04-07 21:32:21 +0200

    images not found

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,31 @@
 'use strict';
 const serverUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-const title = document.querySelector('.js-title');
-const form = document.querySelector('.js-form');
-const searchButton = document.querySelector('.js-button');
-const resetButton = document.querySelector('.js-reset');
-const searchInput = document.querySelector('.js-searchInput');
-const drinkList = document.querySelector('.js-drinkList');
+const title = document.querySelector(".js-title");
+const form = document.querySelector(".js-form");
+const searchButton = document.querySelector(".js-button");
+const resetButton = document.querySelector(".js-reset");
+const searchInput = document.querySelector(".js-searchInput");
+const drinkList = document.querySelector(".js-drinkList");
+let allDrinks = [];
 
 fetch(serverUrl)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-    let drinks = JSON.stringify (data.drinks); 
-  console.log(data.drinks);
-});
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    allDrinks = data.drinks;
+    console.log(allDrinks);
+    renderDrinks();
+  });
+
+//Funciones:
+function renderDrinks() {
+  let html = '';
+  for (const drink of allDrinks) {
+    html += `<li class ="drink js-drink" id=${drink.idDrink}>`;
+    html += `<h2>${drink.strDrink}</h2>`;
+    html += `<img src=${drink.strImageSource}`;
+    html += `</li>`;
+  }
+  drinkList.innerHTML = html;
+}
