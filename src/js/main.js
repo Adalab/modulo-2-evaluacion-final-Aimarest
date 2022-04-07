fad34454adec1f84fef4d46381b5060277c5296e
Aimarest <amarcosestevez@gmail.com>
2022-04-07 21:42:56 +0200

    images found

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-const serverUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const serverUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const title = document.querySelector(".js-title");
 const form = document.querySelector(".js-form");
 const searchButton = document.querySelector(".js-button");
@@ -24,7 +24,7 @@ function renderDrinks() {
   for (const drink of allDrinks) {
     html += `<li class ="drink js-drink" id=${drink.idDrink}>`;
     html += `<h2>${drink.strDrink}</h2>`;
-    html += `<img src=${drink.strImageSource}`;
+    html += `<img src="${drink.strDrinkThumb}"/>`;
     html += `</li>`;
   }
   drinkList.innerHTML = html;
