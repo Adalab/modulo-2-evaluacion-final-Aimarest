1c6d5b19d722483d05469376bd8fe2c3d06111ba
Aimarest <amarcosestevez@gmail.com>
2022-04-10 20:49:20 +0200

    paint favourites

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,15 @@
 const inputSearch = document.querySelector(".js-searchInput");
 const buttonSearch = document.querySelector(".js-button");
 const drinksList = document.querySelector(".js-drinkList");
-const urlApi = "https:/www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-const favourites = document.querySelector(".js-favourites");
-
+const urlApi = "www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const listOfFavourites = document.querySelector(".js-favourites");
+let favourites = [];
 //Función manejadora del evento 'click' en el botón de buscar bebidas.
 
 function handleClickSearch() {
   const inputValue = inputSearch.value;
   let allDrinks = [];
-  fetch(`${urlApi}${inputValue}`)
+  fetch(`https://${urlApi}${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       allDrinks = data.drinks;
@@ -24,28 +24,37 @@ function handleClickSearch() {
         };
         return wDrinks;
       });
-      console.log(wantedDrinks);
       paintDrinks(wantedDrinks, drinksList);
-      drinksList.addEventListener("click", makeFavourites);
+      // la variable drink contiene el objeto javascript con la informacion para pintar
+      for (const drink of wantedDrinks) {
+        // currentDrink contiene el li al que quiero agregar el evento click
+        const currentDrink = document.getElementById(drink.id);
+        currentDrink.addEventListener("click", function () {
+          //Busco la bebida si está en favoritos, para añadirlo o no:
+          const favouriteDrinkIndex = favourites.findIndex(
+            (fav) => fav.id === drink.id
+          );
+          if (favouriteDrinkIndex === -1) {
+            //Devuelve -1 porque no lo encontró en el listado, por lo que hago el push para añadirlo.
+            favourites.push(drink);
+          } else {
+            favourites.splice(favouriteDrinkIndex, 1); //Busco la posición de la bebida, y lo elimino.
+          }
+          paintDrinks(favourites, listOfFavourites);
+        });
+      }
     });
-}
-//Creo una función que valga para pintar en una lista del HTML cada objeto de un array como un li.
-
-function paintDrinks(list, listDOM) {
-  let html = "";
-  for (const li of list) {
-    html += `<li class="drink js-drink" id=${li.id}>`;
-    html += `<h2 class= "drink__title"> ${li.name}</h2>`;
-    html += `<img class= "drink__image" src="${li.image}"/>`;
-    html += `</li>`;
+  //Creo una función que valga para pintar en una lista del HTML cada objeto de un array como un li.
+  function paintDrinks(list, listDOM) {
+    let html = "";
+    for (const li of list) {
+      html += `<li class="drink js-drink" id=${li.id}>`;
+      html += `<h2 class= "drink__title"> ${li.name}</h2>`;
+      html += `<img class= "drink__image" src="${li.image}"/>`;
+      html += `</li>`;
+    }
+    listDOM.innerHTML = html;
   }
-  listDOM.innerHTML = html;
-}
-//Función manejadora del evento de escucha del click en un elemento de la lista:
-
-function makeFavourites(event) {
-  console.log("holis caracolis");
-  console.log(event.target);
 }
 //Eventos:
 
