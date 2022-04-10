2792fb9e6991bd539adc8bfa894603c936a81f30
Aimarest <amarcosestevez@gmail.com>
2022-04-10 12:08:35 +0200

    first commit in new exercise

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
+// 1- Traer los elementos del HTML que necesito usar en js.
 
-
+const inputSearch = document.querySelector(".js-searchInput");
+const buttonSearch = document.querySelector(".js-button");
+function handleClickSearch() {
+  const inputValue = inputSearch.value;
+}
+buttonSearch.addEventListener("click", handleClickSearch);
