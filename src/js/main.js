1c72f89b0adef416f0dfda35ad90b7146c824e48
Aimarest <amarcosestevez@gmail.com>
2022-04-10 11:47:59 +0200

    last version

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+// Elementos que necesito traer del html:
+
 const serverUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const title = document.querySelector(".js-title");
 const form = document.querySelector(".js-form");
@@ -12,9 +14,9 @@ let favourites = [];
 
 //Funciones:
 
-function renderDrinks(list, drinks) {
+function renderDrinks(list, allDrinks) {
   let html = "";
-  for (const drink of drinks) {
+  for (const drink of allDrinks) {
     html += `<li class ="drink js-drink" id=${drink.idDrink}>`;
     html += `<h2>${drink.strDrink}</h2>`;
     html += `<img src="${drink.strDrinkThumb}"/>`;
@@ -36,7 +38,21 @@ function filterDrinks() {
       for (const drink of allDrinks) {
         const currentDrink = document.getElementById(drink.idDrink);
         currentDrink.addEventListener("click", function () {
-          renderFavouriteDrink(currentDrink);
+          //renderFavouriteDrink(currentDrink);
+
+          //Busco la bebida si está en favoritos, para añadirlo o no:
+          // const currentDrinkObject = allDrinks.find(item => item.idDrink === currentDrink.id)
+          const favouriteDrinkIndex = favourites.findIndex((fav) => {
+            return fav === currentDrink;
+          });
+          if (favouriteDrinkIndex === -1) {
+            //Devuelve -1 porque no lo encontró en el listado, por lo que hago el push para añadirlo.
+            favourites.push(currentDrink);
+          } else {
+            favourites.splice(favouriteDrinkIndex, 1); //Busco la posición de la bebida, y lo elimino.
+          }
+          debugger;
+          renderDrinks(favouritesList, favourites);
         });
       }
     });
@@ -47,22 +63,8 @@ function resetFilter(event) {
   drinkList.innerHTML = "";
   searchInput.value = "";
 }
-function renderFavouriteDrink(currentDrink) {
-  console.log(currentDrink);
-  renderDrinks();
-}
-//Busco la bebida si está en favoritos, para añadirlo o no:
-
-const favouriteDrinkIndex = favourites.findIndex((fav) => {
-  return fav === currentDrink;
-});
-if (favouriteDrinkIndex === -1) {
-  //Devuelve -1 porque no lo encontró en el listado, por lo que hago el push para añadirlo.
-  favourites.push(currentDrink);
-} else {
-  favourites.splice(favouriteDrinkIndex, 1); //Busco la posición de la bebida, y lo elimino.
-}
-searchButton.addEventListener("click", filterDrinks);
-resetButton.addEventListener("click", resetFilter);
 
 //Eventos:
+
+searchButton.addEventListener("click", filterDrinks);
+resetButton.addEventListener("click", resetFilter);
